2f3016c01faae09d665971e61fc12dd550db5026
Theeicon45 <judahkinyanjui@gmail.com>
2024-10-15 15:34:26 +0300

    new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-Red-400'>
+      <h1 className=''> 
+      Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
