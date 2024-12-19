7c6f722ccb8933eac05bd5c9a18bb05099114530
Theeicon45 <judahkinyanjui@gmail.com>
2024-12-19 22:05:57 +0300

    table

--- src/AdminPages/AdmissionManagement.jsx
+++ src/AdminPages/AdmissionManagement.jsx
@@ -1,11 +1,45 @@
-
+import { Table } from 'antd';
 
 const AdmissionManagement = () => {
+  const columns=[
+  {
+    title:'Name',
+    dataIndex:'name'
+  },
+  {
+    title:'Age',
+    dataIndex:'age'
+  },
+  {
+    title:'Adress',
+    dataIndex:'adress'
+  }
+
+];
+const dataSource =[]
+for (let index=1; index<17; index++){
+  dataSource.push({
+    key:index,
+    name:"Name"+index,
+    age:index,
+    adress:"Adress"+index,
+    description:"Description"+index
+  })
+}
   return (
     <div className="">
-      AdmissionManagement
-      <h1>ghuhiu</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo ducimus impedit molestias quidem, consequatur modi repellendus eaque nihil sed consectetur atque necessitatibus ipsum laboriosam reprehenderit hic itaque dolorem possimus eum pariatur rerum vero quia corrupti. Tempora earum est corporis cupiditate, voluptates harum atque deserunt necessitatibus doloribus? Aliquam debitis porro pariatur ut odio totam, eos expedita. Fuga expedita tempore commodi pariatur ipsa magni quae, minima molestias ex veritatis dolorum provident neque iusto deserunt! Ratione nisi maiores unde ad odio, natus qui deleniti minus quaerat magni eligendi voluptatibus, numquam fuga iure atque tempora repellendus earum eum mollitia quasi! Laboriosam, saepe! Tempore, voluptates!</p>
+        <Table 
+        columns={columns}
+        dataSource={dataSource}
+        expandable={{rowExpandable:(record)=>true,
+          expandedRowRender:(record)=> {
+            return <p>{record.description}</p>
+          }
+
+        }}
+        >
+
+        </Table>
     </div>
   )
 }
--- src/Components/Sidepannel.jsx
+++ src/Components/Sidepannel.jsx
@@ -1,9 +1,20 @@
-import { Link,useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import { FaTachometerAlt, FaUserGraduate, FaChalkboardTeacher, FaMoneyBillWave, FaCommentDots, FaChartBar, FaFolderOpen, FaCog, FaAngleRight,FaAngleLeft } from 'react-icons/fa';
-import { MdSchool, MdLogout } from 'react-icons/md';
-import { AiOutlineInfoCircle,  } from 'react-icons/ai';
-import { IconBlack } from '../Utils/images';
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import {
+  FaTachometerAlt,
+  FaUserGraduate,
+  FaChalkboardTeacher,
+  FaMoneyBillWave,
+  FaCommentDots,
+  FaChartBar,
+  FaFolderOpen,
+  FaCog,
+  FaAngleRight,
+  FaAngleLeft,
+} from "react-icons/fa";
+import { MdSchool, MdLogout } from "react-icons/md";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { IconBlack } from "../Utils/images";
 
 const Sidepannel = () => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -17,82 +28,101 @@ const Sidepannel = () => {
   const handleMouseEnter = () => {
     if (!isSticky) setIsExpanded(true);
   };
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    // Clear session data (e.g., local storage)
-    localStorage.removeItem('authToken'); // replace 'authToken' with your token or session key
-    // Redirect to login page
-    navigate('/Login');
-  };
 
   const handleMouseLeave = () => {
     if (!isSticky) setIsExpanded(false);
   };
-  
+
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("authToken"); // replace 'authToken' with your token or session key
+    navigate("/Login");
+  };
+
+  // Define an array of links
+  const navigationLinks = [
+    {
+      to: "/DashboardOverview",
+      icon: <FaTachometerAlt />,
+      label: "Dashboard Overview",
+    },
+    {
+      to: "/AdmissionManagement",
+      icon: <MdSchool />,
+      label: "Admission Management",
+    },
+    {
+      to: "/StudentManagement",
+      icon: <FaUserGraduate />,
+      label: "Student Management",
+    },
+    {
+      to: "/StaffManagement",
+      icon: <FaChalkboardTeacher />,
+      label: "Staff Management",
+    },
+    {
+      to: "/FinanceManagement",
+      icon: <FaMoneyBillWave />,
+      label: "Finance Management",
+    },
+    {
+      to: "/CommunicationCenter",
+      icon: <FaCommentDots />,
+      label: "Communication Center",
+    },
+    { to: "/ReportAnalytics", icon: <FaChartBar />, label: "Report Analytics" },
+    {
+      to: "/ResourceSharing",
+      icon: <FaFolderOpen />,
+      label: "Resource Sharing",
+    },
+    { to: "/HelpCenter", icon: <AiOutlineInfoCircle />, label: "Help Center" },
+    { to: "/Settings", icon: <FaCog />, label: "Settings" },
+  ];
 
   return (
-    <div className="flex ">
+    <div className="flex">
       {/* Sidebar Container */}
       <div
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         className={`transition-all duration-300 ease-in-out ${
-          isExpanded ? 'w-48' : 'w-16'
-        }  h-screen  fixed flex flex-col`}
-        style={{ zIndex: '10', marginRight: isExpanded ? '10px' : '0' }}
+          isExpanded ? "w-48" : "w-16"
+        } h-screen fixed flex flex-col`}
+        style={{ zIndex: "10", marginRight: isExpanded ? "10px" : "0" }}
       >
         {/* Logo and Title */}
-        <div className={`flex items-center mb-8  ${isExpanded ? 'justify-start' : 'justify-center'}`}>
+        <div
+          className={`flex items-center mb-8 ${
+            isExpanded ? "justify-start" : "justify-center"
+          }`}
+        >
           <img src={IconBlack} alt="Educonnect Logo" className="w-10 h-10" />
-          {isExpanded && <p className="text-cyan-400 text-xl ml-2">Educonnect</p>}
+          {isExpanded && (
+            <p className="text-cyan-400 text-xl ml-2">Educonnect</p>
+          )}
         </div>
 
-        {/* Navigation Links with Hidden Scrollbar */}
-        <div className="scrollable flex flex-col overflow-y-auto bg-scroll gap-6 h-full pl-2 ">
-          <Link to="/DashboardOverview" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <FaTachometerAlt className="text-xl" />
-            {isExpanded && <span>Dashboard <br />Overview</span>}
-          </Link>
-          <Link to="/AdmissionManagement" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <MdSchool className="text-xl" />
-            {isExpanded && <span>Admission  <br/> Management</span>}
-          </Link>
-          <Link to="/StudentManagement" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <FaUserGraduate className="text-xl" />
-            {isExpanded && <span>Student <br/>  Management</span>}
-          </Link>
-          <Link to="/StaffManagement" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <FaChalkboardTeacher className="text-xl" />
-            {isExpanded && <span>Staff  <br/> Management</span>}
-          </Link>
-          <Link to="/FinanceManagement" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <FaMoneyBillWave className="text-xl" />
-            {isExpanded && <span>Finance <br/>  Management</span>}
-          </Link>
-          <Link to="/CommunicationCenter" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <FaCommentDots className="text-xl" />
-            {isExpanded && <span>Communication  <br/> Center</span>}
-          </Link>
-          <Link to="/ReportAnalytics" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <FaChartBar className="text-xl" />
-            {isExpanded && <span>Report  <br/> Analytics</span>}
-          </Link>
-          <Link to="/ResourceSharing" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <FaFolderOpen className="text-xl" />
-            {isExpanded && <span>Resource  <br/> Sharing</span>}
-          </Link>
-          <Link to="/HelpCenter" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <AiOutlineInfoCircle className="text-xl" />
-            {isExpanded && <span>Help  <br/> Center</span>}
-          </Link>
-          <Link to="/Settings" className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full">
-            <FaCog className="text-xl" />
-            {isExpanded && <span>Settings</span>}
-          </Link>
+        {/* Navigation Links */}
+        <div className="scrollable flex flex-col overflow-y-auto bg-scroll gap-6 h-full pl-2">
+          {navigationLinks.map((link, index) => (
+            <Link
+              key={index}
+              to={link.to}
+              className="flex items-center gap-4 hover:bg-tahiti-100 rounded-md w-full"
+            >
+              <span className="text-xl">{link.icon}</span>
+              {isExpanded && <span>{link.label}</span>}
+            </Link>
+          ))}
         </div>
 
         {/* Logout Button */}
-        <button onClick={handleLogout } className="mt-4 flex items-center gap-4 text-Red-500 p-4 hover:bg-Orange-100">
+        <button
+          onClick={handleLogout}
+          className="mt-4 flex items-center gap-4 text-Red-500 p-4 hover:bg-Orange-100"
+        >
           <MdLogout className="text-xl" />
           {isExpanded && <span>Logout</span>}
         </button>
@@ -100,21 +130,21 @@ const Sidepannel = () => {
         {/* Toggle Button */}
         <button
           onClick={toggleSticky}
-          className="absolute right-0 top-4 -mr-6 p-1  transition-all"
+          className="absolute right-0 top-4 -mr-6 p-1 transition-all"
         >
           {isExpanded ? <FaAngleLeft /> : <FaAngleRight />}
         </button>
-      
       </div>
-
 
       {/* Main Content Area */}
       <div
-        className={`transition-all duration-300 ease-in-out`}
-        style={{ marginLeft: isExpanded ? '200px' : '70px', paddingRight: '10px' }}
+        className="transition-all duration-300 ease-in-out"
+        style={{
+          marginLeft: isExpanded ? "200px" : "70px",
+          paddingRight: "10px",
+        }}
       >
-        {/* Content here dynamically adjusts based on sidebar's state */}
-       
+        {/* Content dynamically adjusts based on sidebar's state */}
       </div>
     </div>
   );
